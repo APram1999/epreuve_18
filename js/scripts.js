241f9523d2629d16830f8869e8d5107132f5b754
APram1999 <35166493+APram1999@users.noreply.github.com>
2018-10-18 23:37:54 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ 		$('#rules1').hide();
+ 		$('#rules').mouseover(function(){
+ 			$('#rules1').show();
+ 		});
+ 		$('#rules').mouseout(function(){
+ 			$('#rules1').hide();
+ 		})
+ 		
+ 	});
